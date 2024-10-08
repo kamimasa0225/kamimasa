ef56b51a64c49a6ebca8aa165358dbd84c2ba161
kamimasa0225 <kamimasa0225ksj@naver.com>
2024-10-09 05:40:55 +0900

    Update server.js

--- Active/Active/server.js
+++ Active/Active/server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 정적 파일 제공
-app.use(express.static(path.join(dirname, '../Active')));
+app.use(express.static(path.join(__dirname, '../Active')));
 
 // 기본 라우트
 app.get('/', (req, res) => {
-  res.sendFile(path.join(dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 서버 시작
